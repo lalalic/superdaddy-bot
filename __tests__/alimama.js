49fb186f15ad1ebd6f3fb0bfd9bf949338f33c8f
lalalic <lalalic@139.com>
2016-12-27 13:45:59 +0800

    timeout cache

--- __tests__/alimama.js
+++ __tests__/alimama.js
@@ -1,13 +1,30 @@
-const {convert}=require("../lib/alimama")
+const {convert, getBrowser}=require("../lib/alimama")
 
 const itemUrl="https://world.taobao.com/item/542225473068.htm?fromSite=main&ali_refid=a3_430620_1006:1124293071:N:%E7%94%B7t%E6%81%A4:8c63de4fa5b7a86d0481b1d169481650&ali_trackid=1_8c63de4fa5b7a86d0481b1d169481650&spm=a312a.7700714.0.0.JhWX1T"
 const convertedUrl="https://s.click.taobao.com/t?e=m%3D2%26s%3D40wt4W4bUgIcQipKwQzePOeEDrYVVa64Qih%2F7PxfOKS5VBFTL4hn2RjNzjCClKKaHGUKWrwhgPnVTWkqIto4v3oWoz7Mm2I4xgB7KBWgH2FXCw9E8H0f51yL4MavYz5of79lZflCzgRQBTAHr9VzocYMXU3NNCg%2F"
 describe("alimama service", function(){
-	it(`can convert ${itemUrl}`, function(){
+	
+	it("connectivity", function(){
+		const serviceUrl="https://login.taobao.com/member/login.jhtml?style=mini&from=alimama&redirectURL=http%3A%2F%2Flogin.taobao.com%2Fmember%2Ftaobaoke%2Flogin.htm%3Fis_login%3d1&full_redirect=true&disableQuickLogin=true"
+    
+		return getBrowser()
+		.then(browser=>{
+			return browser.get(serviceUrl).catch(e=>console.error(e.message))
+				.then(loaded=>{
+					return browser.executeScript(function(_selector){
+						return document.querySelector(_selector).innerHTML
+					},"div")
+				})
+				.then(html=>console.log(html))
+				.then(a=>browser.quit())
+		})
+	},30*1000)
+	
+	fit(`can convert ${itemUrl}`, function(){
 		return convert(itemUrl,{"推广类型":"网站推广","导购名称":"wechat","推广位名称":"个人推荐"})
 			.then(converted=>{
 				expect(typeof(converted)).toBe("string")
 				expect(converted.substr(-20)).toEqual(convertedUrl.substr(-20))
 			})
-	}, 90*1000)
+	}, 60*1000)
 })
